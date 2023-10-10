1d179da20a67a0d213b93735ceb13b34321b7804
Yong Hyeon Jeon <101920006+Raccooon98@users.noreply.github.com>
2023-10-11 02:58:37 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -2,18 +2,16 @@
 #include<string>
 
 using namespace std;
-
+int freq[26];
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	string s;
 	cin >> s;
-	for(char a='a';a<='z';a++){
-        int cnt=0;
-        for(auto c:s){
-            if(a==c)
-                cnt++;
-        }
-        cout<<cnt<<' ';
+    for(auto c:s){
+        freq[c-'a']++;
+    }
+    for(int i=0;i<26;i++){
+        cout<<freq[i]<<' ';
     }
 	}
