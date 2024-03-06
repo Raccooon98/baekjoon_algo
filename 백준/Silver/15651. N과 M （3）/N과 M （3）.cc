d8fac7aa173ec00e9de49fcb35a1aec794edd132
Yong Hyeon Jeon <101920006+Raccooon98@users.noreply.github.com>
2024-03-06 21:25:11 +0900

    [Silver III] Title: N과 M (3), Time: 360 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int N, M;
+vector<int> arr;
+bool vis[10];
+
+
+void DFS(int n) {
+	if (n == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	else {
+
+		for (int i = 1; i <= N; i++) {
+			arr.push_back(i);
+			vis[i] = 1;
+			DFS(n + 1);
+			arr.pop_back();
+			vis[i] = 0;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N >> M;
+	DFS(0);
+}
