fbc4d4e5e8652e461124b60c1b21d7bcd844722a
Yong Hyeon Jeon <101920006+Raccooon98@users.noreply.github.com>
2024-05-11 13:09:01 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 124 ms, Memory: 4380 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include<tuple>
+#include <algorithm>
+using namespace std;
+
+int V, E, result;
+int parent[10001];
+
+int find(int x) {
+	if (parent[x] == x)return x;
+	else return parent[x] = find(parent[x]);
+}
+void Union(int x, int y) {
+	x = find(x);
+	y = find(y);
+	parent[y] = x;
+}
+bool sameparent(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y)return true;
+	else return false;
+}
+int main() {
+	
+	cin >> V >> E;
+	
+	vector<tuple<int,int,int>>v;
+	for (int i = 0; i < E; i++) {
+		int from, to, cost;
+		cin >> from >> to >> cost;
+		v.push_back({ cost,from,to });
+	}
+	sort(v.begin(), v.end());
+	for (int i = 1; i <= V; i++)parent[i] = i;
+	for (int i = 0; i < v.size(); i++) {
+		if (!sameparent(get<1>(v[i]), get<2>(v[i]))) {
+			Union(get<1>(v[i]), get<2>(v[i]));
+			result += get<0>(v[i]);
+		}
+	}
+	cout << result;
+}
