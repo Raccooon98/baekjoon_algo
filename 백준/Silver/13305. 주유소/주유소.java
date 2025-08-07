53f48b5c195a98ceb9af340f795389ed2c56edeb
Yong Hyeon Jeon <101920006+Raccooon98@users.noreply.github.com>
2025-08-08 01:14:57 +0900

    [Silver III] Title: 주유소, Time: 316 ms, Memory: 30400 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,35 @@
+//어차피 N번째 지점에선 기름 못넣음
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int cost[], dist[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        cost = new int[N+1];
+        dist = new int[N+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=N-1;i++){
+            dist[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=N;i++){
+            cost[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum=0, mincost=cost[1];
+
+        for(int i=1;i<N;i++){
+            if(cost[i]<mincost){
+                mincost=cost[i];
+            }
+            sum+=mincost*dist[i];
+        }
+        System.out.println(sum);
+
+    }
+}
