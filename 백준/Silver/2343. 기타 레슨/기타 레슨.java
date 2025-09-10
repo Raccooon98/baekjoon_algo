a850e15d88b4cb8b06326e4af23e0fadd2f9d0e1
Yong Hyeon Jeon <101920006+Raccooon98@users.noreply.github.com>
2025-09-11 01:42:09 +0900

    [Silver I] Title: 기타 레슨, Time: 276 ms, Memory: 24064 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,51 +1,48 @@
-//결국 이분탐색 결정문제
-//답이 될 수 있는 값을 이분탐색으로 찾기
-//각 블루레이에 넣을 수 있는지는 그리디하게 판단해보기
 import java.io.*;
 import java.util.*;
 
 public class Main {
     static int N, M;
-    static int lecture[];
+    static int arr[];
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
 
-        lecture = new int[N];
         st = new StringTokenizer(br.readLine());
-
-        int left = 0;
-        int right = 0;
+        int left = 0, right = 0;
         for (int i = 0; i < N; i++) {
-            lecture[i] = Integer.parseInt(st.nextToken());
-            left = Math.max(left, lecture[i]);
-            right += lecture[i];
+            arr[i] = Integer.parseInt(st.nextToken());
+            left = Math.max(left, arr[i]);
+            right += arr[i];
         }
 
+        int ans = 0;
 
-        int ans = right;
         while (left <= right) {
-            int mid = (left + right) / 2;
-            int sum = 0, count = 1;
+            int mid = left + (right - left) / 2;
+            int sum = 0;
+            int count = 1;
 
             for (int i = 0; i < N; i++) {
-                if (sum + lecture[i] > mid) {
+                if (sum + arr[i] > mid) {
                     count++;
                     sum = 0;
                 }
-                sum += lecture[i];
+                sum += arr[i];
             }
 
-            if (count > M) left = mid + 1;
+            if (count > M)
+                left = mid + 1;
             else {
                 ans = mid;
                 right = mid - 1;
             }
         }
 
-        System.out.println(left);
+        System.out.println(ans);
     }
 }
