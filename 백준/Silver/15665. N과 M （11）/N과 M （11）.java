9a3619a3bbec9d0e3615b0106176d71e7e832af5
Yong Hyeon Jeon <101920006+Raccooon98@users.noreply.github.com>
2025-11-27 21:23:06 +0900

    [Silver II] Title: N과 M (11), Time: 344 ms, Memory: 34204 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+
+public class Main {
+    static int[] input,arr;
+    static int N,M;
+    static boolean[] vis;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N  = Integer.parseInt(st.nextToken());
+        M  = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        vis = new boolean[N];
+        input = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0;i<N;i++){
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+
+        DFS(0);
+
+        System.out.println(sb.toString());
+    }
+
+    static void DFS(int depth){
+        if(depth == M){
+            for(int n:arr){
+                sb.append(n).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int temp =-1;
+        for(int i=0;i<N;i++){
+            int now = input[i];
+            if(temp == now) continue;
+            else{
+                temp = now;
+                arr[depth] = now;
+                DFS(depth+1);
+            }
+        }
+    }
+}
