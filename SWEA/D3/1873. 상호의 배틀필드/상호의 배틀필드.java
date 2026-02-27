b186beaeff95e757bbe3bf4a3c38ee1185c47f9d
Yong Hyeon Jeon <101920006+Raccooon98@users.noreply.github.com>
2026-02-28 00:07:29 +0900

    [D3] Title: 상호의 배틀필드, Time: 98 ms, Memory: 28,032 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -1,138 +1,176 @@
+import java.util.*;
+import java.io.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-class Solution
-{
-	static int[] dx = {-1, 1, 0, 0};
+public class Solution {
+    static int W, H, N;
+    static char[][] board;
+    static char[] command;
+    static int dir = 0;
+    static Tank tank;
+    static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static char[]directoions = {'^','v','<','>'};
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스의 개수
-        
-        for(int test_case = 1; test_case <= T; test_case++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int H = Integer.parseInt(st.nextToken()); // 게임 맵의 높이
-            int W = Integer.parseInt(st.nextToken()); // 게임 맵의 너비
-            
-            // 맵 입력 받기
-//            String[][] map = new String[H][W];
-            char[][] map = new char[H][W];
-            int car_x = -1, car_y = -1, dir = -1;
-            for(int i = 0; i < H; i++) {
-//                st = new StringTokenizer(br.readLine());
-                char[] input = br.readLine().toCharArray();
-                for(int j = 0; j < W; j++) {
-                    map[i][j] = input[j];
-                    
-                    // 초기 전차의 위치와 방향 탐색
-                    switch(map[i][j]) {
-                    case '^':
-                        car_x = i;
-                        car_y = j;
-                        dir = 0;
-                        break;
-                    case 'v':
-                        car_x = i;
-                        car_y = j;
-                        dir = 1;
-                        break;
-                    case '<':
-                        car_x = i;
-                        car_y = j;
-                        dir = 2;
-                        break;
-                    case '>':
-                        car_x = i;
-                        car_y = j;
-                        dir = 3;
-                        break;
-                    }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            H = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+
+            board = new char[H][W];
+
+            for (int i = 0; i < H; i++) {
+                String input = br.readLine();
+                for (int j = 0; j < W; j++) {
+                    board[i][j] = input.charAt(j);
+                    if (board[i][j] == '^' || board[i][j] == 'v' || board[i][j] == '<' || board[i][j] == '>')
+                        tank = new Tank(i, j, board[i][j]);
                 }
             }
-            
-            int N = Integer.parseInt(br.readLine()); // 입력의 개수
-            String str = br.readLine(); // 사용자의 입력들
-            
-            for(int i = 0; i < str.length(); i++) {
-                
-                int nx, ny;
-                char order=str.charAt(i);
-                
-                if(order=='S') {
-                	nx = car_x;
-                    ny = car_y;
-                    
-                    // 다음 위치가 맵 범위를 벗어나면 패스
-                    
-                    
-                    while(true) {
-                    	nx += dx[dir];
-                        ny += dy[dir];
-                        if(nx < 0 || nx >= H || ny < 0 || ny >= W) {
-                            break;
-                        }
-                    	if(map[nx][ny] == '#') { // 포탄이 강철 벽을 만나면
-                            break; // 포탄 소멸 (아무 일도 일어나지 않음)
-                        }
-                        else if(map[nx][ny] == '*') { // 포탄이 벽돌 벽을 만나면
-                            map[nx][ny] = '.'; // 벽이 파괴되어 평지로 바뀐 후
-                            break; // 포탄 소멸
-                        }
-                    }
-                    
+
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            command = new char[N];
+
+            String input = br.readLine();
+            for (int n = 0; n < N; n++) {
+                command[n] = input.charAt(n);
+            }
+
+            for (char c : command) {
+                action(c);
+            }
+
+            sb.append("#").append(t).append(" ");
+            for (int i = 0; i < H; i++) {
+                for (int j = 0; j < W; j++) {
+                    sb.append(board[i][j]);
                 }
-                else {
-                	switch(order) {
-                    case 'U': // 윗칸이 평지이면 위로 이동
-                        dir = 0; // 전차의 방향을 위쪽으로 변경후
-                        
-                        break;
-                        
-                    case 'D': // 아랫칸이 평지이면 아래로 이동
-                        dir = 1; // 전차의 방향을 아래쪽으로 변경
-                        
-                        break;
-                        
-                    case 'L': // 왼쪽이 평지이면 왼쪽으로 이동
-                        dir=2;
-                        break;
-                        
-                    case 'R': // 오른쪽이 평지이면 오른쪽으로 이동
-                        dir=3;
-                        break;
-                    }
-                    map[car_x][car_y] = directoions[dir];
-                    
-                    nx = car_x + dx[dir];
-                    ny = car_y + dy[dir];
-                    
-                    // 다음 위치가 맵 범위를 벗어나면 패스
-                    if(nx < 0 || nx >= H || ny < 0 || ny >= W) {
-                        continue;
-                    }
-                    
-                    if(map[nx][ny] == '.') { // 평지이면 이동
-                        map[nx][ny] = directoions[dir];
-                        map[car_x][car_y] = '.';
-                        
-                        car_x = nx;
-                        car_y = ny;
-                    }
-                    
+                sb.append("\n");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+    static void action(char c) {
+        int dir = getdir(tank.dir);
+        int x = tank.x;
+        int y = tank.y;
+
+        if (c == 'U') {
+            int nx = x + dx[0];
+            int ny = y + dy[0];
+
+            if (nx < 0 || ny < 0 || nx >= H || ny >= W || board[nx][ny] == '*' || board[nx][ny] == '#' || board[nx][ny] == '-') {
+                board[x][y] = '^';
+                tank.dir = '^';
+                return;
+            }
+
+            tank.x = nx;
+            tank.y = ny;
+            board[x][y] = '.';
+            board[nx][ny] = '^';
+            tank.dir = '^';
+            return;
+        } else if (c == 'D') {
+            int nx = x + dx[1];
+            int ny = y + dy[1];
+
+            if (nx < 0 || ny < 0 || nx >= H || ny >= W || board[nx][ny] == '*' || board[nx][ny] == '#' || board[nx][ny] == '-') {
+                board[x][y] = 'v';
+                tank.dir = 'v';
+                return;
+            }
+
+            tank.x = nx;
+            tank.y = ny;
+            board[x][y] = '.';
+            board[nx][ny] = 'v';
+            tank.dir = 'v';
+            return;
+        } else if (c == 'L') {
+            int nx = x + dx[2];
+            int ny = y + dy[2];
+
+            if (nx < 0 || ny < 0 || nx >= H || ny >= W || board[nx][ny] == '*' || board[nx][ny] == '#' || board[nx][ny] == '-') {
+                board[x][y] = '<';
+                tank.dir = '<';
+                return;
+            }
+
+            tank.x = nx;
+            tank.y = ny;
+            board[x][y] = '.';
+            board[nx][ny] = '<';
+            tank.dir = '<';
+            return;
+        } else if (c == 'R') {
+            int nx = x + dx[3];
+            int ny = y + dy[3];
+
+            if (nx < 0 || ny < 0 || nx >= H || ny >= W || board[nx][ny] == '*' || board[nx][ny] == '#' || board[nx][ny] == '-') {
+                board[x][y] = '>';
+                tank.dir = '>';
+                return;
+            }
+
+            tank.x = nx;
+            tank.y = ny;
+            board[x][y] = '.';
+            board[nx][ny] = '>';
+            tank.dir = '>';
+            return;
+        } else if (c == 'S') {
+            int nx = x, ny = y;
+            while (true) {
+                nx += dx[dir];
+                ny += dy[dir];
+
+                if (nx < 0 || ny < 0 || nx >= H || ny >= W || board[nx][ny] == '#') {
+                    nx -= dx[dir];
+                    ny -= dy[dir];
+                    return;
+                }
+
+                if (board[nx][ny] == '*') {
+                    board[nx][ny] = '.';
+                    return;
                 }
             }
-            
-            System.out.print("#" + test_case + " ");
-            for(int i = 0; i < H; i++) {
-                for(int j = 0; j < W; j++) {
-                    System.out.print(map[i][j]);
-                }
-                System.out.println();
-            }
+        }
+    }
+
+    static int getdir(char c) {
+        int dir=0;
+
+        if (c == '^') {
+            dir = 0;
+        } else if (c == 'v') {
+            dir = 1;
+        } else if (c == '<') {
+            dir = 2;
+        } else if (c == '>') {
+            dir = 3;
+        }
+
+        return dir;
+    }
+
+    static class Tank {
+        int x;
+        int y;
+        char dir;
+
+        public Tank(int x, int y, char dir) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
         }
     }
 }
