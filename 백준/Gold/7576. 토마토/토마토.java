599524ca717ec9e4f29c44efd4ce43f1106f9a51
Yong Hyeon Jeon <101920006+Raccooon98@users.noreply.github.com>
2026-03-08 19:06:01 +0900

    [Gold V] Title: 토마토, Time: 576 ms, Memory: 104144 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,10 +1,11 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
     static int N, M;
+    static Queue<int[]> q = new ArrayDeque<>();
     static int[][] board;
-    static Queue<Info> q = new ArrayDeque();
+    static int[][] vis;
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
 
@@ -19,20 +20,22 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-
             for (int j = 0; j < M; j++) {
-                int num = Integer.parseInt(st.nextToken());
-                board[i][j] = num;
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-                if (num == 1) {
-                    q.offer(new Info(i, j));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (board[i][j] == 1) {
+                    q.offer(new int[]{i, j});
                 }
             }
         }
 
         int ans = BFS();
-
         boolean check = true;
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (board[i][j] == 0) {
@@ -41,45 +44,32 @@ public class Main {
             }
         }
 
-        if (!check) {
-            System.out.println(-1);
-        } else {
-            System.out.println(ans - 1);
-        }
+        System.out.println(check == true ? ans - 1 : -1);
     }
 
-    private static int BFS() {
+    static int BFS() {
         int max = -1;
 
         while (!q.isEmpty()) {
-            Info cur = q.poll();
-            int x = cur.x;
-            int y = cur.y;
-            max = Math.max(max, board[x][y]);
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+            max = Math.max(max, board[cx][cy]);
 
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
 
                 if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if (board[nx][ny] == -1) continue;
                 if (board[nx][ny] != 0) continue;
+                if (board[nx][ny] == -1) continue;
 
-                board[nx][ny] = board[x][y] + 1;
+                board[nx][ny] = board[cx][cy] + 1;
+                q.offer(new int[]{nx, ny});
 
-                q.offer(new Info(nx, ny));
             }
         }
 
         return max;
-    }
-
-    private static class Info {
-        int x, y;
-
-        public Info(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 }
